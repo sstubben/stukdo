86e9acc943f4fb369d7dc92ee2fe54df226de9a6
Simon Stubben <simonstubben@gmail.com>
2015-02-03 11:35:54 +0800

    Added about and gitignore

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test "should get about" do
     get :home
+    assert_response :success
+  end
+end
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
     assert_response :success
   end
 
